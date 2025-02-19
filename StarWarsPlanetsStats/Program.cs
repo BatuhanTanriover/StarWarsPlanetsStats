7f4cf3f75e1d2b9deeda0cd16ac4ecdbc0c6387b
Batuhan Tanrıöver <batu_2000_10@hotmail.com>
2025-02-19 19:37:35 +0300

    Add project files.

---
+++ StarWarsPlanetsStats/Program.cs
@@ -0,0 +1,40 @@
+﻿using StarWarsPlanetsStats.ApiDataAccess;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+ApiDataReader apiDataReader = new ApiDataReader();
+var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
+
+
+var root = JsonSerializer.Deserialize<Root>(json);
+
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+
+
+
+public record Result(
+       [property: JsonPropertyName("name")] string name,
+       [property: JsonPropertyName("rotation_period")] string rotation_period,
+       [property: JsonPropertyName("orbital_period")] string orbital_period,
+       [property: JsonPropertyName("diameter")] string diameter,
+       [property: JsonPropertyName("climate")] string climate,
+       [property: JsonPropertyName("gravity")] string gravity,
+       [property: JsonPropertyName("terrain")] string terrain,
+       [property: JsonPropertyName("surface_water")] string surface_water,
+       [property: JsonPropertyName("population")] string population,
+       [property: JsonPropertyName("residents")] IReadOnlyList<string> residents,
+       [property: JsonPropertyName("films")] IReadOnlyList<string> films,
+       [property: JsonPropertyName("created")] DateTime created,
+       [property: JsonPropertyName("edited")] DateTime edited,
+       [property: JsonPropertyName("url")] string url
+   );
+
+public record Root(
+    [property: JsonPropertyName("count")] int count,
+    [property: JsonPropertyName("next")] string next,
+    [property: JsonPropertyName("previous")] object previous,
+    [property: JsonPropertyName("results")] IReadOnlyList<Result> results
+);
