816890ab7fc2c37891d625c32f40eb973858a4ea
Batuhan Tanrıöver <batu_2000_10@hotmail.com>
2025-02-20 21:04:42 +0300

    İmperial March

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -1,12 +1,22 @@
-﻿using StarWarsPlanetsStats.ApiDataAccess;
+﻿
+using StarWarsPlanetsStats.ApiDataAccess;
+using StarWarsPlanetsStats.DTOs;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+try
+{
+    await new StarWarsPlanetsStatsApp(
+        new ApiDataReader(), new MockStarWarsApiDataReader()
+        ).Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred" +
+        "Exception message:" + ex.Message +
+        "Stack Trace: " + ex.StackTrace);
+}
 
-ApiDataReader apiDataReader = new ApiDataReader();
-var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
 
 
-var root = JsonSerializer.Deserialize<Root>(json);
 
 
 Console.WriteLine("Press any key to close.");
@@ -14,27 +24,163 @@ Console.ReadKey();
 
 
 
+public class StarWarsPlanetsStatsApp
+{
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
 
-public record Result(
-       [property: JsonPropertyName("name")] string name,
-       [property: JsonPropertyName("rotation_period")] string rotation_period,
-       [property: JsonPropertyName("orbital_period")] string orbital_period,
-       [property: JsonPropertyName("diameter")] string diameter,
-       [property: JsonPropertyName("climate")] string climate,
-       [property: JsonPropertyName("gravity")] string gravity,
-       [property: JsonPropertyName("terrain")] string terrain,
-       [property: JsonPropertyName("surface_water")] string surface_water,
-       [property: JsonPropertyName("population")] string population,
-       [property: JsonPropertyName("residents")] IReadOnlyList<string> residents,
-       [property: JsonPropertyName("films")] IReadOnlyList<string> films,
-       [property: JsonPropertyName("created")] DateTime created,
-       [property: JsonPropertyName("edited")] DateTime edited,
-       [property: JsonPropertyName("url")] string url
-   );
+    public StarWarsPlanetsStatsApp(IApiDataReader apiDataReader, IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
 
-public record Root(
-    [property: JsonPropertyName("count")] int count,
-    [property: JsonPropertyName("next")] string next,
-    [property: JsonPropertyName("previous")] object previous,
-    [property: JsonPropertyName("results")] IReadOnlyList<Result> results
-);
+
+    public async Task Run()
+    {
+        string? json = null;
+        try
+        {
+
+            json = await _apiDataReader.Read("https://swapi.dev/", "api/planets");
+
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("API request was unsuccessful." +
+                "Switching to mock data." +
+                "Exception message:" + ex.Message);
+
+        }
+        if (json is null)
+        {
+
+            json = await _secondaryApiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+
+        var planets = ToPlanets(root);
+
+        foreach (var planet in planets)
+        {
+            Console.WriteLine(planet);
+        }
+        Console.WriteLine();
+        Console.WriteLine(
+            "The statistics of which property would you" +
+            "like to see? ");
+        Console.WriteLine("population");
+        Console.WriteLine("diameter");
+        Console.WriteLine("surface water");
+
+        var userChoice = Console.ReadLine();
+        if (userChoice == "population")
+        {
+            ShowStatistics(planets, "population", planet => planet.Population);
+
+        }
+        else if (userChoice == "diameter")
+        {
+            ShowStatistics(planets, "diameter", planet => planet.Diameter);
+        }
+        else if (userChoice == "surface water")
+        {
+            ShowStatistics(planets, "surface water", planet => planet.SurfaceWater);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice");
+        }
+
+
+    }
+    private void ShowStatistics(IEnumerable<Planet> planets, string propertyName, Func<Planet, int?> propertySelector)
+    {
+        var planetWithMaxPropertyValue =
+                planets.MaxBy(propertySelector);
+
+        Console.WriteLine($"Max {propertyName} is: " +
+            $"{propertySelector(planetWithMaxPropertyValue)} " +
+            $"planet: ({planetWithMaxPropertyValue.Name})");
+
+        var planetWithMinPropertyValue =
+            planets.MinBy(propertySelector);
+
+        Console.WriteLine($"Min {propertyName} is: " +
+            $"{propertySelector(planetWithMinPropertyValue)} " +
+            $"planet: ({planetWithMinPropertyValue.Name})");
+    }
+
+
+
+    private IEnumerable<Planet> ToPlanets(Root? root)
+    {
+        if (root == null)
+        {
+            throw new ArgumentNullException(nameof(root));
+        }
+        var planets = new List<Planet>();
+
+        foreach (var planetDto in root.results)
+        {
+
+            Planet planet = (Planet)planetDto;
+            planets.Add(planet);
+        }
+
+
+        return planets;
+    }
+}
+
+public readonly record struct Planet
+{
+    public string Name { get; }
+    public int Diameter { get; }
+    public int? SurfaceWater { get; }
+    public int? Population { get; }
+
+    public Planet(string name, int diameter, int? surfaceWater, int? population)
+    {
+        if (name is null)
+        {
+            throw new ArgumentNullException(nameof(name));
+
+        }
+
+
+        Name = name;
+        Diameter = diameter;
+        SurfaceWater = surfaceWater;
+        Population = population;
+    }
+
+    public static explicit operator Planet(Result planetDto)
+    {
+        var name = planetDto.name;
+        var diameter = int.Parse(planetDto.diameter);
+
+        int? population = planetDto.population.ToIntOrNull();
+        int? surfaceWater = planetDto.surface_water.ToIntOrNull();
+
+        return new Planet(name, diameter, surfaceWater, population);
+    }
+
+
+}
+
+public static class StringExtensions
+{
+    public static int? ToIntOrNull(this string? input)
+    {
+        int? result = null;
+        if (int.TryParse(input, out int resultParsed))
+        {
+            result = resultParsed;
+        }
+
+        return result;
+    }
+}
